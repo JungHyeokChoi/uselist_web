ad2f4e85af53ffec746158874b13a9ee48873d2e
JungHyeok Choi <page1114@gamil.com>
2020-06-17 12:18:38 +0900

    Making Server

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser');
 var app = express()
 
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
+app.get('/hello', (req, res) => {
+    console.log("Hello World")
+    console.log(req.body)
+    res.render('hello.ejs')
+})
+
 var port = 8080
-app.listen(port, function (req, res) {
+app.listen(port, (req, res) => {
     console.log(`server is Running at http://localhost:8080`)
 });
